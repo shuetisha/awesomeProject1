1d9178cab1b2eabc3a8f926c9537e9c188926964
korhankeser <korhan@resmo.com>
2023-05-11 15:43:45 +0300

    testing 2

--- main.go
+++ main.go
@@ -2,12 +2,16 @@ package main
 
 import (
 	"awesomeProject1/config"
+	"flag"
 	"os"
 )
 
 func main() {
 	println("test")
 	var config config.Testing
+
+	flag.StringVar(&config.Test, "testing test", "", "testing only")
+
 	config.Test = os.Getenv("testingTest")
 	println(config.Test)
 }
