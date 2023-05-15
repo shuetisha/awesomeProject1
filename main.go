f8abfeee04803cf286fab2cf2f1adfa8559293b1
korhankeser <korhan@resmo.com>
2023-05-15 09:38:59 +0300

    testing 2

--- main.go
+++ main.go
@@ -5,6 +5,10 @@ import (
 	"os"
 )
 
+var (
+	version = "dev"
+)
+
 func main() {
 	println("test")
 	var config config.Testing
@@ -12,4 +16,6 @@ func main() {
 	config.Test = os.Getenv("TESTING_TEST")
 
 	println(config.Test)
+
+	println("app version is: %s", version)
 }
